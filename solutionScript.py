b9c06647995419956dcdd07dc88f423a221e1bba
Wilson <luke.usa93@gmail.com>
2023-11-10 13:11:26 -0500

    added more comprensive comments and improved readability

--- solutionScript.py
+++ solutionScript.py
@@ -1,21 +1,13 @@
 import os
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-dictionary_path = os.path.join(current_directory, "dictionary.txt")
-
 class Node:
     def __init__(self):
         self.children = {}
         self.word = False
 
-
 class Trie:
     def __init__(self):
         self.root = Node()
-        self.size = 0
-
-    def __len__(self):
-        return self.size
 
     def insert(self, word: str) -> None:
         root = self.root
@@ -24,63 +16,71 @@ class Trie:
             if ch not in root.children:
                 root.children[ch] = Node()
             root = root.children[ch]
-
-        self.size += 1
-        root.word = True   
+        root.word = True
 
 
 class Solution:
     def __init__(self):
+        # Initialize trie
         self.trie = Trie()
-        self.result = ''
-        self.resultPath = []
-        self.resultPoints = 0
+        self.result = {
+            'word': '',
+            'path': [],
+            'points': 0,
+            'swaps': []
+        }
+
         self.rows = 5
         self.cols = 5
+        self.letterValues = letterValues = {'A': 1,'E': 1,'I': 1,'O': 1,'N': 2,'R': 2,'S': 2,'T': 2,'D': 3,'G': 3,'L': 3,'B': 4,'H': 4,'P': 4,'M': 4,'U': 4,'Y': 4,'C': 5,'F': 5,'V': 5,'W': 5,'K': 6,'J': 7,'X': 7,'Q': 8,'Z': 8}
+
+        #Adds all the words from out dictionary.txt file to the trie
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        dictionary_path = os.path.join(current_directory, "dictionary.txt")
 
         with open(dictionary_path, "r") as file:
             dictionary = file.readlines()
             for word in dictionary:
                 self.trie.insert(word.strip().upper())
 
-    def findWords(self, board, doubleLetter, doublePoint, tripleLetter, skips):
+    def findWords(self, board, doubleLetter, doublePoint, tripleLetter, swaps):
         self.doubleLetter = doubleLetter
         self.doublePoint = doublePoint
         self.tripleLetter = tripleLetter
 
+        #go through entire board and dfs to find viable words
         for i in range(self.rows):
             for j in range(self.cols):
-                self.dfs(board, i, j, [], self.trie.root, 0, '', skips)
+                self.dfs(board, i, j, [], self.trie.root, 0, '', swaps)
 
-        return [self.result, self.resultPath, self.resultPoints]
+        #return highest score word
+        return self.result
     
-    def dfs(self, board, i, j, path, root, points, currentWord, skips, isSkip = False):
+    def dfs(self, board, i, j, path, root, points, currentWord, swaps, isSwap = False):
         char = board[i][j]
 
         # Check if current character is in trie
-        if skips != 0 and isSkip == False:
+        if swaps != 0 and isSwap == False:
+            # if swap is available and we have not already swapped this letter, swap the letter and dfs every other possible path
             for key in root.children:
                 if key == char:
                     continue
 
                 original = board[i][j]
                 board[i][j] = key
-                self.dfs(board, i, j, path, root, points, currentWord, skips - 1, True)
+                self.dfs(board, i, j, path, root, points, currentWord, swaps - 1, True)
                 board[i][j] = original
 
-
+        #move down trie
         root = root.children.get(char)
         if root is None:
             return
 
-
-        letterValues = {'A': 1,'E': 1,'I': 1,'O': 1,'N': 2,'R': 2,'S': 2,'T': 2,'D': 3,'G': 3,'L': 3,'B': 4,'H': 4,'P': 4,'M': 4,'U': 4,'Y': 4,'C': 5,'F': 5,'V': 5,'W': 5,'K': 6,'J': 7,'X': 7,'Q': 8,'Z': 8} 
-
-        # Update path and mark visited
+        # Update variables and mark visited
         path = path + [(i, j)]
-        points += letterValues[char]
+        points += self.letterValues[char]
         currentWord = currentWord + char
-        board[i][j] = letterValues[char]
+        board[i][j] = False
 
         # Add to solution if node is marked as a word in trie
         if root.word:
@@ -92,70 +92,77 @@ class Solution:
             if len(currentWord) >= 6:
                 longWordBonus = 10
 
+            #check for bonus points
             if self.doubleLetter in path:
-                doubleLetterBonus = board[self.doubleLetter[0]][self.doubleLetter[1]]
+                letterIndex = path.index(self.doubleLetter)
+                doubleLetterBonus = self.letterValues[currentWord[letterIndex]]
             if self.tripleLetter in path:
-                tripleLetterBonus = board[self.tripleLetter[0]][self.tripleLetter[1]] * 2
+                letterIndex = path.index(self.tripleLetter)
+                tripleLetterBonus = self.letterValues[currentWord[letterIndex]]
             if self.doublePoint in path:
                 doublePointBonus = 2
 
+            #calculate total score and replaced result if higher
             wordTotalScore = (points + doubleLetterBonus + tripleLetterBonus) * doublePointBonus + longWordBonus
-            if wordTotalScore > self.resultPoints:
-                self.result = currentWord
-                self.resultPath = path
-                self.resultPoints = wordTotalScore
+            if wordTotalScore > self.result['points']:
+                self.result['word'] = currentWord
+                self.result['path'] = path
+                self.result['points'] = wordTotalScore
 
+        # DFS in all directions
 
         # Right
-        if j + 1 < self.cols and board[i][j+1] and type(board[i][j+1]) != int:
-            self.dfs(board, i, j+1, path, root, points, currentWord, skips)
+        if j + 1 < self.cols and board[i][j+1]:
+            self.dfs(board, i, j+1, path, root, points, currentWord, swaps)
         
         #Left
-        if j - 1 >= 0 and board[i][j-1] and type(board[i][j-1]) != int:
-            self.dfs(board, i, j-1, path, root, points, currentWord, skips)
+        if j - 1 >= 0 and board[i][j-1]:
+            self.dfs(board, i, j-1, path, root, points, currentWord, swaps)
 
         # Down
-        if i + 1 < self.rows and board[i+1][j] and type(board[i+1][j]) != int:
-            self.dfs(board, i+1, j, path, root, points, currentWord, skips)
+        if i + 1 < self.rows and board[i+1][j]:
+            self.dfs(board, i+1, j, path, root, points, currentWord, swaps)
 
         # Up
-        if i - 1 >= 0 and board[i-1][j] and type(board[i-1][j]) != int:
-            self.dfs(board, i-1, j, path, root, points, currentWord, skips)
+        if i - 1 >= 0 and board[i-1][j]:
+            self.dfs(board, i-1, j, path, root, points, currentWord, swaps)
 
         #Diagonals
 
-        if j + 1 < self.cols and i + 1 < self.rows and type(board[i+1][j+1]) != int:
-            self.dfs(board, i + 1, j + 1, path, root, points, currentWord, skips)
+        if j + 1 < self.cols and i + 1 < self.rows:
+            self.dfs(board, i + 1, j + 1, path, root, points, currentWord, swaps)
         
-        if j + 1 < self.cols and i - 1 >= 0 and type(board[i-1][j+1]) != int:
-            self.dfs(board, i - 1, j + 1, path, root, points, currentWord, skips)
+        if j + 1 < self.cols and i - 1 >= 0:
+            self.dfs(board, i - 1, j + 1, path, root, points, currentWord, swaps)
 
-        if j - 1 >= 0 and i + 1 < self.rows and type(board[i+1][j-1]) != int:
-            self.dfs(board, i + 1, j - 1, path, root, points, currentWord, skips)
+        if j - 1 >= 0 and i + 1 < self.rows:
+            self.dfs(board, i + 1, j - 1, path, root, points, currentWord, swaps)
 
-        if j - 1 >= 0 and i - 1 >= 0 and type(board[i-1][j-1]) != int:
-            self.dfs(board, i - 1, j - 1, path, root, points, currentWord, skips)
+        if j - 1 >= 0 and i - 1 >= 0:
+            self.dfs(board, i - 1, j - 1, path, root, points, currentWord, swaps)
         
+        # Reset board to original state
         board[i][j] = char
 
+    #function to reset result data
     def resetData(self):
-        self.result = ''
-        self.resultPath = []
-        self.resultPoints = 0
-        self.doubleLetter = None
-        self.doublePoint = None
-        self.tripleLetter = None
+        self.result = {
+            'word': '',
+            'path': [],
+            'points': 0,
+            'swaps': []
+        }
 
-    def solve(self, board, doubleLetter, tripleLetter, doublePoint, skips):
-        result = self.findWords(board, doubleLetter, doublePoint, tripleLetter, skips)
+    #function to solve the board and return the result with swap data
+    def solve(self, board, doubleLetter, tripleLetter, doublePoint, swaps):
+        result = self.findWords(board, doubleLetter, doublePoint, tripleLetter, swaps)
         self.resetData()    
 
         swappedLetters = []
-        for i in range(len(result[0])):
-            letter, coords = result[0][i], result[1][i]
+        for i in range(len(result['word'])):
+            letter, coords = result['word'][i], result['path'][i]
             if letter != board[coords[0]][coords[1]]:
-                swappedLetters.append([coords[0], coords[1], letter, board[coords[0]][coords[1]]])
+                swappedLetters.append((coords[0], coords[1], letter, board[coords[0]][coords[1]]))
 
-        result.append(swappedLetters)
+        result['swaps'] = swappedLetters
         return result
-
--- solver-gui.py
+++ solver-gui.py
@@ -13,6 +13,7 @@ class SolvingWindow:
         self.oneSwapResult = None
         self.twoSwapResult = None
 
+    #remove all highlighting from the board, keeps bonus square highlighting
     def clearBoard(self):
         for row in self.grid_labels:
             for cell in row:
@@ -23,17 +24,19 @@ class SolvingWindow:
                 self.grid_labels[self.highlighted_squares[key][0]][self.highlighted_squares[key][1]].config(bg=key)
 
     
+    #revert all letters to their original state for new input or swap selection
     def revertLetters(self):
         for result_data in [self.noSwapResult, self.oneSwapResult, self.twoSwapResult]:
             if result_data:
-                swapData = result_data[3]
+                swapData = result_data['swaps']
                 for i, j, swapToLetter, swapFromLetter in swapData:
                     self.grid_labels[i][j].config(fg='#C5C6C7', text=swapFromLetter)
 
-
+    #handle key presses
     def on_key(self, event): 
         self.clearBoard()
 
+        #handle movement of cell selection with keyboard arrow key and backspace movements
         if event.keysym == "Up":
             current_row, current_col = self.current_cell
             self.grid_labels[current_row][current_col].config(relief="raised")
@@ -89,6 +92,7 @@ class SolvingWindow:
             self.current_cell = (current_row, current_col)
             self.grid_labels[current_row][current_col].config(relief="sunken")
 
+        #handle letter input
         elif event.char.isalpha() and len(event.char) == 1:
             self.grid_labels[self.current_cell[0]][self.current_cell[1]].config(text=event.char.upper())
             self.move_to_next_cell()
@@ -98,6 +102,7 @@ class SolvingWindow:
             self.oneSwapResult = None
             self.twoSwapResult = None
 
+    #manage movement of cell selection on board
     def move_to_next_cell(self):
         current_row, current_col = self.current_cell
         self.grid_labels[current_row][current_col].config(relief="raised")
@@ -110,6 +115,7 @@ class SolvingWindow:
         self.current_cell = (current_row, current_col)
         self.grid_labels[current_row][current_col].config(relief="sunken")
 
+    #handle cell clicks
     def on_cell_click(self, row, col):
         self.clearBoard()
 
@@ -118,10 +124,12 @@ class SolvingWindow:
         self.grid_labels[row][col].config(relief="sunken")
 
     def solve_grid(self):
+        #extract the board letters and solve the board
         self.revertLetters()
         board = self.extract_board_letters()
         doubleLetter, tripleLetter, doublePoint = self.highlighted_squares['blue'], self.highlighted_squares['red'], self.highlighted_squares['purple']
 
+        #solve the board for each swap option and update the UI
         noSwapSolution = self.answer.solve(board, doubleLetter, tripleLetter, doublePoint, 0)
         self.noSwapResult = noSwapSolution
         self.update_result(noSwapSolution, 0)
@@ -135,25 +143,28 @@ class SolvingWindow:
         self.update_result(twoSwapSolution, 2)
 
     def extract_board_letters(self):
+        #extract the letters from the board for solving
         board_letters = []
         for row_labels in self.grid_labels:
             row_letters = [label.cget("text") for label in row_labels]
             board_letters.append(row_letters)
         return board_letters
 
+    #function to manage changes in bonus point locations
     def highlight_square(self, color):
-
+        #results with vary with bonus point locations so data must be reset
         self.noSwapResult = None
         self.oneSwapResult = None
         self.twoSwapResult = None
 
         def set_highlight_color():
+            #manage highlighting of bonus point locations
             selected_cell = self.current_cell
             if selected_cell:
                 row, col = selected_cell
 
                 if not self.highlighted_squares[color] is None:
-                    self.grid_labels[self.highlighted_squares[color][0]][self.highlighted_squares[color][1]].config(relief="raised", bg='#1F2833')
+                    self.grid_labels[self.highlighted_squares[color][0]][self.highlighted_squares[color][1]].config(releif = 'raised', bg='#1F2833')
 
                 if self.highlighted_squares[color] == (row, col):
                     self.grid_labels[row][col].config(relief='sunken')
@@ -170,26 +181,30 @@ class SolvingWindow:
         return set_highlight_color
 
     def update_result(self, result_data, numSwaps):
+        #update result data on UI
         if numSwaps == 0:
-            self.result_no_swap_label.config(text=f"No Swap: {result_data[0]} - {result_data[2]} points")
+            self.result_no_swap_label.config(text=f"No Swap: {result_data['word']} - {result_data['points']} points")
         if numSwaps == 1:
-            self.result_one_swap_label.config(text=f"One Swap: {result_data[0]} - {result_data[2]} points")
+            self.result_one_swap_label.config(text=f"One Swap: {result_data['word']} - {result_data['points']} points")
         if numSwaps == 2:
-            self.result_two_swap_label.config(text=f"Two Swap: {result_data[0]} - {result_data[2]} points")
+            self.result_two_swap_label.config(text=f"Two Swap: {result_data['word']} - {result_data['points']} points")
         
-
+        #if the swap option matches current result data, highlight the path and swaps
         if numSwaps == self.swap_value.get():
-            letterCoords = result_data[1]
+            letterCoords = result_data['path']
             self.grid_labels[letterCoords[0][0]][letterCoords[0][1]].config(bg='#77889e')
 
             for letter in letterCoords[1:]:
                 self.grid_labels[letter[0]][letter[1]].config(bg='#7ea2cf')
             
-            for i, j, swapToLetter, swapFromLetter in result_data[3]:
+            for i, j, swapToLetter, swapFromLetter in result_data['swaps']:
                 self.grid_labels[i][j].config(fg='#FF0000', text=(swapToLetter))
+
+        #have to update the UI after every update or the UI will freeze until all three results are calculated
         self.app.update()
 
     def on_swap_selection(self):
+        #when the user selects a swap option, update the board to reflect the result
         self.clearBoard()
         self.revertLetters()
 
@@ -198,21 +213,22 @@ class SolvingWindow:
         result_data = varMap[currentValue]
 
         if result_data:
-            letterCoords = result_data[1]
+            letterCoords = result_data['path']
             self.grid_labels[letterCoords[0][0]][letterCoords[0][1]].config(bg='#77889e')
 
             for letter in letterCoords[1:]:
                 self.grid_labels[letter[0]][letter[1]].config(bg='#7ea2cf')
             
-            for i, j, swapToLetter, swapFromLetter in result_data[3]:
+            for i, j, swapToLetter, swapFromLetter in result_data['swaps']:
                 self.grid_labels[i][j].config(fg='#FF0000', text=(swapToLetter))
 
     def startWindow(self):
+        #initialize the board and create all needed UI elements
         grid_labels = self.grid_labels
 
         app = tk.Tk()
         self.app = app
-        app.title("Letter Grid")
+        app.title("SpellCaster Solver")
 
         for i in range(5):
             row_labels = []
